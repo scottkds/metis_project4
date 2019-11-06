810723ac36725913be1d641ebbb2b94af41f77de
scottkds <skroegs@gmail.com>
2019-11-05 22:42:46 -0800

    Added named upler, got get_posts working.

--- src/pushshift_io.py
+++ src/pushshift_io.py
@@ -2,28 +2,41 @@
 import requests
 import json
 from math import ceil, floor
-from datetime import datetime, date, time
+from datetime import datetime, date, time, timedelta
+from collections import namedtuple
+
+post = namedtuple('post', ['id', 'author', 'title', 'url', 'img', 'created'])
+post('123', 'Jim', 'Stuff and things', 'here', 'there', 'today')
 
 # Function to convert a timestamp to a unix timestamp
 def unix_time(date_time):
     epoch = datetime(year=1970, month=1, day=1, hour=0, minute=0, second=0)
-    return int((datetime.strptime(date_time, '%m/%d/%Y %H:%M:%S') - epoch).total_seconds())
-    # return floor((datetime.strptime(date_time, '%m/%d/%Y %H:%M:%S')).timestamp())
+    # return int((datetime.strptime(date_time, '%m/%d/%Y %H:%M:%S') - epoch).total_seconds())
+    return floor((datetime.strptime(date_time, '%m/%d/%Y %H:%M:%S')).timestamp())
 
 def get_posts(subreddit, before_str, after_str, count):
     """Returns a list of reponses from the Pushshift.io API"""
     request_string = 'https://api.pushshift.io/reddit/search/submission?subreddit={}&before={}&after={}&size=25'
     request_string = request_string.format(subreddit, unix_time(before_str), unix_time(after_str))
-    print(request_string)
     response = requests.get(request_string)
     assert response.status_code == 200
     return json.loads(response.content)
 
-rs = get_posts('FoodPorn', '11/01/2019 00:00:00','01/01/2019 00:00:00', 25)
+rs = get_posts('FoodPorn', '11/01/2019 00:00:00','01/01/2018 00:00:00', 25)
 
-len(rs['data'])
+
+l_data = []
+for p in rs['data']:
+    l_data.append(post(p['id'], p['author'], p['title'], p['permalink'], p['url'], p['created_utc']))
+l_data
 
 (datetime.now().timestamp())
+# timestamp 1572591600
+# minus epoch 1572566400
+
+datetime.fromtimestamp(unix_time('11/01/2019 00:00:00'))
+
+datetime(year=2019, month=1, day=1) - timedelta(seconds=1)
 
 unix_time('11/01/2019 00:00:00')
 
@@ -45,4 +58,4 @@ r = requests.get(request_string)
 r
 response_json = json.loads(r.content)
 type(response_json)
-response_json
+response_json['data']
