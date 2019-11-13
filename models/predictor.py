43ee8066b1abc10e91d93c1fbe63cdcdbf18d4d9
scottkds <skroegs@gmail.com>
2019-11-13 13:59:04 -0800

    Removed functionality to predict base on cluster.

--- models/predictor.py
+++ models/predictor.py
@@ -1,7 +1,7 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import NMF
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.metrics.pairwise import cosine_distances
+from sklearn.preprocessing import normalize
 
 from gensim.parsing.preprocessing import preprocess_string, strip_tags
 from gensim.parsing.preprocessing import strip_punctuation, strip_multiple_whitespaces
@@ -17,84 +17,79 @@ def clean_str(string):
 
     Input: A string
     Output: A lists of tokenized words"""
-    rtn = preprocess_string(string, filters=[strip_tags, strip_punctuation,
+    return preprocess_string(string, filters=[strip_tags, strip_punctuation,
                                     strip_numeric, strip_multiple_whitespaces,
                                     strip_short])
-    # print('Clean_str:', rtn)
-    return rtn
 
 def tokenize(string):
     """Cleans and tokenizes a string and returns a list of strings that are in
     VOCABULARY"""
     words_in_string = clean_str(string)
-    rtn = [word for word in words_in_string if word in VOCABULARY]
-    # return [word for word in words_in_string if word in VOCABULARY]
-    # print('tokenize:', rtn)
-    return rtn
+    return [word for word in words_in_string if word in VOCABULARY]
 
-def top_n_similar(n, X, sample_str, vectorizer):
+def top_topic(vectorizer, model, new_document):
+    """Returns the topic most associated with the input string"""
+    word_vec = tf.transform([new_document])
+    topics = nmf.transform(word_vec)
+    return np.argmax(topics)
+
+def top_n_similar(sample_str, n, topic_vector, model, vectorizer):
+    """Returns the top n similar posts to the subreddit forum
+    Inputs:
+        n -> number of posts to return
+        topic_vector -> the topic vector to compare cosine cosine_similarity
+        sample_str -> the input string
+        vectorizer -> the vectorizer used to vectorize the input string
+    Output:
+        numpy array of cosine similarities"""
     Y_tf = vectorizer.transform([sample_str])
-    print(Y_tf.shape)
-    sims = cosine_similarity(X.components_, Y_tf)
+    Y_nmf = nmf.transform(Y_tf)
+    print(Y_nmf.shape)
+    sims = cosine_similarity(topic_vector, Y_nmf)
     sort_order = np.argsort(sims.reshape(sims.shape[0]))[::-1]
     sims_and_idx = zip(sims[sort_order], sort_order)
     # return cosine_similarity(X.components_, Y_tf)
     return list(sims_and_idx)[:n]
 
+def load_pickles():
+    """Loads pickled objects from previous text analisys."""
 
-def make_result_frame(sims_and_idx, model):
-    return pd.DataFrame([np.concatenate((x[0], model.components_[x[1]]), axis=0) for x in sims],
-                 columns=['cosine_similarity'] + tf.get_feature_names())
+    with open('/Users/scott/p4/pickles/vocabulary.pkl', 'rb') as f:
+        VOCABULARY = pickle.load(f)
 
-def make_ordered_result_frame(sims_and_idx, model):
-    return pd.DataFrame([np.concatenate((x[0], model.components_[x[1]]), axis=0) for x in sims],
-                 columns=['cosine_similarity'] + tf.get_feature_names())
+    with open('/Users/scott/p4/pickles/vectorizer_tfidf_NMF_20.pkl', 'rb') as f:
+        tf = pickle.load(f)
 
-with open('/Users/scott/p4/pickles/vocabulary.pkl', 'rb') as f:
-    VOCABULARY = pickle.load(f)
+    with open('/Users/scott/p4/pickles/doc_topic_tfidf_NMF_20.pkl', 'rb') as f:
+        word_vec_reduced = pickle.load(f)
 
-with open('/Users/scott/p4/pickles/vectorizer_tfidf_NMF_20.pkl', 'rb') as f:
-    tf = pickle.load(f)
+    with open('/Users/scott/p4/pickles/model_tfidf_NMF_20.pkl', 'rb') as f:
+        nmf = pickle.load(f)
 
-with open('/Users/scott/p4/pickles/doc_topic_tfidf_NMF_20.pkl', 'rb') as f:
-    word_vec_reduced = pickle.load(f)
-word_vec_reduced.shape
+    with open('/Users/scott/p4/pickles/km.pkl', 'rb') as f:
+        km = pickle.load(f)
 
-with open('/Users/scott/p4/pickles/model_tfidf_NMF_20.pkl', 'rb') as f:
-    nmf = pickle.load(f)
+    return (VOCABULARY, tf, word_vec_reduced, nmf, km)
 
+if __name__ == '__main__':
 
-word_vec_reduced.shape
+    VOCABULARY, tf, word_vec_reduced, nmf, km = load_pickles()
 
-sims = top_n_similar(3, nmf, 'baked chicken with asparagus spears', tf)
-sims
+    test_strings = ['bbq brisket and coleslaw',
+                    'sausage eggs and hashbrowns',
+                    'Check out this christmas spread',
+                    'A nice variety for my son\'s birthday',
+                    'What would good with this steak?']
 
-make_result_frame(sims, nmf)
+    print(top_topic(tf, nmf, 'bbq brisket and coleslaw'))
+    print(top_topic(tf, nmf, 'sausage eggs and hashbrowns'))
+    print(top_topic(tf, nmf, 'Check out this christmas spread'))
+    print(top_topic(tf, nmf, 'A nice variety for my son\'s birthday'))
+    print(top_topic(tf, nmf, 'What would good with this steak?'))
+    print(top_topic(tf, nmf, ''))
 
+    docs = pd.read_csv('/Users/scott/p4/data/interim/fp_posts.csv')
 
-terms = tf.get_feature_names()
-with open('notes.txt', 'w') as notes:
-    for idx, comp in enumerate(lsa.components_):
-        terms_in_components = zip(terms, comp)
-        sorted_terms = sorted(terms_in_components, key=lambda x: x[1], reverse=True)
-        notes.write('Topic {}\n:'.format(idx))
-        for i, term in enumerate(sorted_terms[:10]):
-            notes.write('{} '.format(term[0]))
-        notes.write('\n')
+    top_n_similar(test_strings[1], 5, word_vec_reduced, nmf, tf)
 
-
-preprocess_string('chicken with rice and dumplings', filters=[strip_tags, strip_punctuation,
-                                strip_numeric, strip_multiple_whitespaces,
-                                strip_short])
-
-
-p = {'a': 1, (1,2): [[1,2,3], [4,5,6]]}
-pd.DataFrame(p)
-nmf.components_.shape
-
-step1 = tf.transform(['Baked chicken with broccoli'])
-step1.shape
-step2 = nmf.transform(step1)
-step2.shape
-step2
-word_vec_reduced.shape
+docs.loc[18180]
